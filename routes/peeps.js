0537462ead7eac5f0497e217e2ce4b3d8f9da420
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-06 15:14:36 +0000

    Posting a peep updates page with new page via ajax post request

--- routes/peeps.js
+++ routes/peeps.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models/user');
+const { Peep } = require('../models/peep');
 
 router.get('/', async (req, res) => {
   const user = await User.findByID(req.session['userID']);
@@ -12,6 +13,17 @@ router.get('/', async (req, res) => {
     req.flash('loginForPeeps', 'Login to view peeps');
     res.redirect('/sessions/new');
   }
+});
+
+router.post('/', async (req, res) => {
+  await Peep.create(
+    req.body.text, 
+    req.body.timePosted, 
+    req.session['userID']
+  );
+
+  const peeps = await Peep.getAll();
+  res.send(peeps);
 });
 
 module.exports = router;
